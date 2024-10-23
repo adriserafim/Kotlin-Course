9932271237ebcb1ff9c19b1f8435e27989cfc9f1
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-10-23 17:33:39 -0300

    Foi colocado mais detalhes de formatação de testo e jeitos de utilizar o espaço corretamente

--- AtividadeA.kt
+++ AtividadeA.kt
@@ -1,3 +1,4 @@
+// OBS: O kotem possue margem para você organizar melhor seu códico
 fun main() {
     println("Hello Word") // Como padrão println server para mostrar na tela de execução alguma informação "output"
 
@@ -7,28 +8,34 @@ fun main() {
     product = "iPhone" // Aqui eu estou subescrevendo a informação da variável
     println(product) // Como pode ser visto aqui a variável mudou de infomação
 
-    //var product = "Android" // NÃO pode ser feito isso, isso irá dar uma erro de compilação, pois a variável product já foi criada
+    //var product = "Android" // NÃO pode ser feito isso, isso irá dar uma erro de compilação, pois a variável product
+    // já foi criada
 
     val PI = 3.14 // val é o comando para criar uma variável imutável
     //PI = 2.35 // Sendo aasim caso você faça uma ação para tentar muda-lá o seu programa irá dar um erro de compilação
 
-    var price = 31 // Para colocar valores númerico não precisa colocar o valor entre "", somente variaveis de texto precisão estar entre ""
+    var price = 31 // Para colocar valores númerico não precisa colocar o valor entre "", somente variaveis de texto
+    // precisão estar entre ""
     println(price)
 
-    // Quando você colocar // na linha de código tudo que você escreve depois dessas barra não são intepretadas pelo código e elas
-    // servem para você DOCUMENTAR, REFERENCIAR, FAZER NOTAS sobre o código que você quer referir. Isso é muito importante para organização do seu arquivo
+    // Quando você colocar // na linha de código tudo que você escreve depois dessas barra não são intepretadas
+    // pelo código e elas
+    // servem para você DOCUMENTAR, REFERENCIAR, FAZER NOTAS sobre o código que você quer referir. Isso é muito
+    // importante para organização do seu arquivo
 
     // TODO: Terminar os códigos para zxy // Detendendo do que você escreve a informação pode mudar de cor
 
     println(product::class) // Quando nos colocamos os ::class na função print você vai ter um output de informação
-    println(PI::class) // A infomação que o programa ira te dar nesse caso é que é o tido de variável que é essa que você informou
+    println(PI::class) // A infomação que o programa ira te dar nesse caso é que é o tido de variável que
+    // é essa que você informou
     println(price::class) // Existe varios tipos de variável as mais comuns são double, String, int
 
-    var reminder = "F23" // Quando você utiliza a função var você está deixando o computador escolher automaticamente que tipo de variável ele irá utilizar
+    var reminder = "F23" // Quando você utiliza a função var você está deixando o computador escolher automaticamente
+    // que tipo de variável ele irá utilizar
     // Com isso nem sempre o comutador irá fazer a melhor escolha ou a ecolha que você deseja.
 
-    val number1: Int = 23 // Sé nos quisermos podemos definir na programação qual é o tipo de variável que nos queremos que
-    val text: String = "teste 1" // a variável fique.
+    val number1: Int = 23 // Sé nos quisermos podemos definir na programação qual é o tipo de variável que nos queremos
+    val text: String = "teste 1" // que a variável fique.
     val number2: Double = 23.5 // Existem varios tipos de tipos de variável.
 
     // Quando você não define o tipo da variável ela é do TIPOS INFERIDO
@@ -40,10 +47,12 @@ fun main() {
     val number3: Int = 31 // Ele consegue armazenar até 32-bit de espaço númerico (Usaremos ele 99% do tempo)
     val long1: Long = 192 // Ele consegue armazenar até 64-bit de espaço númerico
 
-    val float1: Float = 31.99f // Ele consegue armazenar até 32-bit de espaço númerico com ponto flutuante "Número quebrado" (É obrigatorio utilizar o f no final)
+    val float1: Float = 31.99f // Ele consegue armazenar até 32-bit de espaço númerico com ponto flutuante "Número
+    // quebrado" (É obrigatorio utilizar o f no final)
     val double: Double = 31.99 // Ele consegue armazenar até 64-bit de espaço númerico com ponto flutuante
 
-    val long2= 192L // Para facilitar nos podemos simplificar e não ter que escrever o tipo na frente você tambem pode usar essa forma para definir o tipo
+    val long2= 192L // Para facilitar nos podemos simplificar e não ter que escrever o tipo na frente você tambem pode
+    // usar essa forma para definir o tipo
     val float2= 31.99f // Long e Float utilizando um L ou um f na frente de uma definição de variável
     println(long2::class)
     println(float2::class)
@@ -58,7 +67,8 @@ fun main() {
     val changingTypeP2 = changingTypeP1.toString()
     println(changingTypeP2::class)
 
-    val long3: Long = 1_200 // Para ficar mais fácil de se ser o número que esta no long você pode colocar um underline para separar a casa do milhar da demais
+    val long3: Long = 1_200 // Para ficar mais fácil de se ser o número que esta no long você pode colocar um underline
+    // para separar a casa do milhar da demais
 
     // Operações matemáticas (+ - / * !)
 
@@ -87,11 +97,22 @@ fun main() {
     // Codificações de texto
 
     val nome : String = "Adriano" // String é uma sequência de chars
-    val nomedomeio : Char = 'S' // Para utilizar o char você tem que utilizar '' em ves de "" ele ocupa menos espaço do que a string, ocupando somente 16 bits
+    val nomedomeio : Char = 'S' // Para utilizar o char você tem que utilizar '' em ves de "" ele ocupa menos espaço do
+    // que a string, ocupando somente 16 bits
     val nomeespaco = '\t' // Utilizando o chars podemos fazer cofigurações especificas como utilizar o espaço do TAB
     val mudadelinha = '\n' // Ou bular a linha
     val nome2 = "Erivan"
-    println(nome+nomeespaco+nomedomeio+mudadelinha+nome2+nomeespaco+nomedomeio)
+    println("Eu sou "+nome+nomeespaco+nomedomeio+mudadelinha+"Já o meu irmão é "+nome2+nomeespaco+nomedomeio) // Pode
+    // ver com isso que fica muito dificil fazer a codificação então temos outro jeito de fazer isso
+
+    println("Eu sou $nome $nomedomeio \n Já o meu irmão é $nome2 $nomedomeio") // Assim o seu codigo fica mais limpo
+    val welcome = """
+        Seja bem vindo ao nosso site!
+        Você é um cliente muito especial para nos! ^^
+    """ // Você támbem pode colocar """ """ o programa imprimir exatamente a formatação que você indicou
+    // Porem perceba que os espaços da formatação de codigo estão ali e vão ser imprimidos juntos com o que você
+    // escreveu. E se você tirar aquela formatação de codigo o seu código vai ficar desorganizado.
+    println(welcome)
 
     // Operando objetos
 
@@ -101,4 +122,27 @@ fun main() {
     val Maxnome = nome.uppercase() // Isso faz com que retorne a variável com todas as letras maiúsculas
     println(Minnome) // Apesar disso também ser uma funsão ela e uma funsão que alterar propriedade
     println(Maxnome) // Diferente da funsão comum que não altera uma variável so utiliza a variálvel para algo
+
+    val welcome2 = """
+        Seja bem vindo ao nosso site!
+        Você é um cliente muito especial para nos! ^^
+    """.trimIndent() // O jeito para resolver o problema do testo relatado anteriormente é utilizando a função
+    // .trimIndent() que vai remover a configuração lateral
+    println(welcome2)
+    val itens = """
+        Item 1;
+        Item 2
+    """.replaceIndent(" - ") // Podemos utilizar outros funções para fazer resultados parecidos, como
+    // a .replaceIndent que vai trocar esse espaço vazio por o caracter que eu escolher
+    println(itens)
+
+    // Funsão lógica
+
+    if (product.length > 5){ // if é a funsão lógica mais básica ele apesar faz uma operação se o resultado for True
+        println("Produto cadastrado")
+    }
+    else{ // Caso o resultado de False ele vai fazer o resurso que estiver no else
+        println("Produto não cadastrado")
+    }
+
 }
