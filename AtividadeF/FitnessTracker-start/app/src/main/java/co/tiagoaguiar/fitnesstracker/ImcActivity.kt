77872c5f1cddca737ee68ff56bd0a2137e7d48e3
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-11-28 13:06:55 -0300

    Foi colocado mas uma tela de caminho

---
+++ AtividadeF/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -0,0 +1,11 @@
+package co.tiagoaguiar.fitnesstracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ImcActivity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
