2b2b8a6a1d554c12d6b7300cca3cf011a98bba2f
Adriano Serafim <adriano_serafim_1@hotmail.com>
2025-01-23 12:35:24 -0300

    Implementado os outros dois metodos de listen to click

--- AtividadeF/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ AtividadeF/FitnessTracker-start/app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -12,7 +12,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(), OnItemClickListener {
+class MainActivity : AppCompatActivity() {
 
 //    private lateinit var btnImc: LinearLayout
     private lateinit var rvMain: RecyclerView
@@ -39,14 +39,52 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
             )
         )
 
-        // Imprementando metodo de RecyclerView
-        val adapter = MainAdapter(mainItems, this)
+//        // 1º Método - Para escutar eventos de click - Implementar interface
+//        // Imprementando metodo de RecyclerView
+//        val adapter = MainAdapter(mainItems, onItemClickListener = this) // Para esse metodo
+//        // funcionar a criação de uma interface tem que ser feita e chamada pela funsão
+//        rvMain = findViewById(R.id.rv_main)
+//        rvMain.adapter = adapter
+////        rvMain.layoutManager = LinearLayoutManager(this) // Isso faz com que o app fique em
+//    // formato de linha
+//        rvMain.layoutManager = GridLayoutManager(this, 2) // Isso faz com que fique
+//    // em formato de grid
+
+//        // 2º Método - Para escutar eventos de click - Implementando a interface via objeto anonimo
+//        val adapter = MainAdapter(mainItems, object : OnItemClickListener {
+//            override fun onClick(id: Int) {
+//                when(id) {
+//                    1 -> {
+//                        val intent = Intent(this@MainActivity, ImcActivity::class.java)
+//                        startActivity(intent)
+//                    }
+//                    2 -> {
+//                        // abrir uma outra activity
+//                    }
+//                }
+//            }
+//        }) // Já para esse modelo a interfase é chamada direta pela activity
+//        rvMain = findViewById(R.id.rv_main)
+//        rvMain.adapter = adapter
+//        rvMain.layoutManager = GridLayoutManager(this, 2)
+
+        // 3º Método - Para escutar eventos de click - Implementando a interface via functions
+        // -- Método mais profisonal --
+        val adapter = MainAdapter(mainItems) { id ->
+            when (id) {
+                1 -> {
+                    val intent = Intent(this@MainActivity, ImcActivity::class.java)
+                    startActivity(intent)
+                }
+
+                2 -> {
+                    // abrir uma outra activity
+                }
+            }
+        } // Já para metodo o evento de toque foi chamado totalmente pelo configuração Kotlin
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
-//        rvMain.layoutManager = LinearLayoutManager(this) // Isso faz com que o app fique em
-    // formato de linha
-        rvMain.layoutManager = GridLayoutManager(this, 2) // Isso faz com que fique
-    // em formato de grid
+        rvMain.layoutManager = GridLayoutManager(this, 2)
 
 //        btnImc = findViewById(R.id.btn_imc)
 //        btnImc.setOnClickListener{
@@ -56,36 +94,75 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
 //        }
     }
 
-    override fun onClick(id: Int) {
-        when(id) {
-            1 -> {
-                val intent = Intent(this, ImcActivity::class.java)
-                startActivity(intent)
-            }
-            2 -> {
-                // abrir uma outra activity
-            }
-        }
-    }
+    // 1º Método - Para escutar eventos de click - Implementar interface
+//    override fun onClick(id: Int) {
+//        when(id) {
+//            1 -> {
+//                val intent = Intent(this, ImcActivity::class.java)
+//                startActivity(intent)
+//            }
+//            2 -> {
+//                // abrir uma outra activity
+//            }
+//        }
+//    }
 
+//    // 1º e 2º Método - Para escutar eventos de click
+//    private inner class MainAdapter(
+//        private val mainItems: List<MainItem>,
+//        private val onItemClickListener: OnItemClickListener
+//        ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+//        // 1 - Qual é o layout XML da celula especifica (item)
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+//            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+//            return MainViewHolder(view)
+//        }
+//
+//        // 2 - Dispara toda vez que houver uma rolagem na tela e for necessario trocar o conteudo
+//        // da celula
+//        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+//            val itemCurrent = mainItems[position]
+//            holder.bind(itemCurrent)
+//        }
+//
+//        // 3 - Informa quantas celulas essa listagem terá
+//        override fun getItemCount(): Int {
+//            return mainItems.size
+//        }
+//
+//        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+//            fun bind(item: MainItem) {
+//                val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+//                val name: TextView = itemView.findViewById(R.id.item_txt_name)
+//                val container: LinearLayout = itemView.findViewById(R.id.item_container_imc)
+//
+//                img.setImageResource(item.drawableId)
+//                name.setText(item.textStringId)
+//                container.setBackgroundColor(item.color)
+//
+//                container.setOnClickListener {
+//                onItemClickListener.onClick(item.id) // Aqui o onItemClickListener é uma
+//                // referência a funsão
+//                }
+//            }
+//        }
+//    }
+
+    // 3º Método - Para escutar eventos de click - Implementando a interface via functions
     private inner class MainAdapter(
         private val mainItems: List<MainItem>,
-        private val onItemClickListener: OnItemClickListener
-        ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
-        // 1 - Qual é o layout XML da celula especifica (item)
+        private val onItemClickListener: (Int) -> Unit,
+    ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             return MainViewHolder(view)
         }
 
-        // 2 - Dispara toda vez que houver uma rolagem na tela e for necessario trocar o conteudo
-        // da celula
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
             val itemCurrent = mainItems[position]
             holder.bind(itemCurrent)
         }
 
-        // 3 - Informa quantas celulas essa listagem terá
         override fun getItemCount(): Int {
             return mainItems.size
         }
@@ -101,7 +178,8 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
                 container.setBackgroundColor(item.color)
 
                 container.setOnClickListener {
-                onItemClickListener.onClick(item.id)
+                    onItemClickListener.invoke(item.id) // Aqui o onItemClickListener é uma
+                // referência a interface
                 }
             }
         }
