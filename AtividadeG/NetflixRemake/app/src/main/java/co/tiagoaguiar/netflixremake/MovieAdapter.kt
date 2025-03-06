222208f7c4cec09ffee2763c0f33cbf9b753b68d
Adriano Serafim <adriano_serafim_1@hotmail.com>
2025-03-06 12:39:28 -0300

    Foi colocado mais detalhe e feito a configuração para deixar a tela de descrição do filme dinamico

--- AtividadeG/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieAdapter.kt
+++ AtividadeG/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MovieAdapter.kt
@@ -4,14 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.model.Movie
 
 // Aqui nos vamos fazer uma class para o adapter para poder separa mais o código, para organizar
 // melhor e deixar mais facil para edições futuras
-class MovieAdapter (private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+class MovieAdapter (
+    private val movies: List<Movie>,
+    @LayoutRes private val layoutId: Int
+    ) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
         return  MovieViewHolder(view)
     } // O layoutInflater. não pode ser utilizado aqui pois essa ferramenta so existe na função
     // AppCompatActivity e como nos não estamos lá não podemos utiliza-lá
