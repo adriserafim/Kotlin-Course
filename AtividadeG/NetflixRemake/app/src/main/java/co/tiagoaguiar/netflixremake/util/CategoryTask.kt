75468df8e764311502dbb63696770a62d309e5c8
Adriano Serafim <adriano_serafim_1@hotmail.com>
2025-03-12 17:16:52 -0300

    Foi ingrementado um metodo mais detalhado e complexo para melhorar o conhecimento sobre o processo de leitura de uma URL

--- AtividadeG/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
+++ AtividadeG/NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
@@ -1,7 +1,10 @@
 package co.tiagoaguiar.netflixremake.util
 
 import android.util.Log
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.Executors
@@ -23,15 +26,35 @@ class CategoryTask {
                 if (statusCode > 400) {
                     throw IOException("Erro na comunicação com o servidor")
                 }
-                // Formula 1: simples e rápida
+//                // Formula 1: Simples e rápida
+//                val stream = urlConnection.inputStream // Sequência bytes
+//                val jsonAsString = stream.bufferedReader().use {it.readText()} // Bytes -> String
+//                Log.i("Teste", jsonAsString)
+
+                // Formula 2: Bytes -> String
                 val stream = urlConnection.inputStream // Sequência bytes
-                val jsonAsString = stream.bufferedReader().use {it.readText()} // Bytes -> String
+                val buffer = BufferedInputStream(stream)
+                val jsonAsString = toString(buffer)
                 Log.i("Teste", jsonAsString)
 
-                // Formula 2: ????
             } catch (e: IOException) {
                 Log.e("Teste", e.message ?: "Erro desconhecido", e)
             }
         }
+    }
+
+    // Formula 2: Bytes -> String
+    private fun toString(stream: InputStream) : String {
+        val bytes = ByteArray(1024)
+        val baos = ByteArrayOutputStream()
+        var read: Int
+        while(true) {
+            read = stream.read(bytes)
+            if (read <=0) {
+                break
+            }
+            baos.write(bytes, 0, read)
+        }
+        return String(baos.toByteArray())
     }
 }
