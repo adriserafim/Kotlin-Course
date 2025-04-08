2059618880f218eb197171a5fde47dc7619b6eb3
Adriano Serafim <adriano_serafim_1@hotmail.com>
2025-04-08 14:40:12 -0300

    Foi entendido o erro do desenvolvedor kkkk, foi feito uma janela de pop-up para adição de imagem

--- AtividadeI/Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/common/view/CustomDialog.kt
+++ AtividadeI/Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/common/view/CustomDialog.kt
@@ -2,37 +2,65 @@ package co.tiagoaguiar.course.instagram.common.view
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import co.tiagoaguiar.course.instagram.R
 
 class CustomDialog(context: Context) : Dialog(context) {
     private lateinit var dialogLinearLayout: LinearLayout
     private lateinit var txtButtons: Array<TextView>
+    private lateinit var txtTitle: TextView
+    private var titleId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_custom)
         dialogLinearLayout = findViewById(R.id.dialog_container)
+        txtTitle = findViewById(R.id.dialog_title)
     }
 
-    fun addButton(listener: View.OnClickListener, vararg texts:Int) {
+    override fun setTitle (titleId: Int) {
+        this.titleId = titleId
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun addButton(vararg texts:Int, listener: View.OnClickListener) {
         txtButtons = Array(texts.size) {
-            TextView(context)
+            TextView(context).apply {
+                setText(texts[it])
+                setTextColor(context.getColor(R.color.gray_text))
+                textSize = 16f
+//                setPadding(30,50,30,50)
+//                setOnClickListener(listener) // Colocar o Listener aqui vai facilitar a leitura,
+            //                mas pode dificultar no designer
+            }
         }
         texts.forEachIndexed { index, txtId ->
+            txtButtons[index].id = txtId
             txtButtons[index].setText(txtId)
+            txtButtons[index].setOnClickListener {
+                listener.onClick(it)
+                dismiss()
+            }
         }
     }
 
     override fun show() {
         super.show()
 
+        titleId?.let {
+            txtTitle.setText(it)
+        }
+
         for(textView in txtButtons) {
-            dialogLinearLayout.addView(textView)
+            val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            layoutParams.setMargins(30, 50, 30, 50)
+            dialogLinearLayout.addView(textView, layoutParams)
         }
     }
 }
--- AtividadeI/Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
+++ AtividadeI/Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,9 +1,13 @@
 package co.tiagoaguiar.course.instagram.register.view
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import co.tiagoaguiar.course.instagram.R
 import co.tiagoaguiar.course.instagram.common.view.CustomDialog
@@ -17,11 +21,31 @@ class RegisterPhotoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register_photo, container,false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val customDialog = CustomDialog(requireContext())
-        customDialog.addButton({
-        }, R.string.photo, R.string.gallery)
+        customDialog.setTitle(R.string.app_name) // Aqui nos conseguimos trocar o texto do titulo
+        // do customDialog
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+//            Log.i("Teste", (it as TextView).text.toString())
+            when (it.id) {
+                R.string.photo -> {
+                    // Faz alguma coisa
+                    Log.i("Teste", "Foto")
+                }
+                R.string.gallery -> {
+                    // Faz outra coisa
+                    Log.i("Teste", "Galeria")
+                }
+            }
+        }
+
+        // Foi troca o Lisiner de lugar causando um erro, pois tem que mater a seguencia do chamdo,
+        // mas com isso faz com que o codigo fique mais fácil de ler e deixar mais profissional
+//        customDialog.addButton({
+//            Log.i("Teste", (it as TextView).text.toString())
+//        }, R.string.photo, R.string.gallery)
         customDialog.show()
     }
 }
