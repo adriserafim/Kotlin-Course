c1b2e6a14021033c2fcdfdb858659ffbe1eafbeb
Adriano Serafim <adriano_serafim_1@hotmail.com>
2025-04-11 12:02:55 -0300

    Foi modificado a sistensbar para a cor clara quando ela esta na tela principal

--- AtividadeI/Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
+++ AtividadeI/Instagram/app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
@@ -1,12 +1,20 @@
 package co.tiagoaguiar.course.instagram.main.view
 
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import co.tiagoaguiar.course.instagram.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+        window.statusBarColor = ContextCompat.getColor(this,R.color.gray)
     }
 }
